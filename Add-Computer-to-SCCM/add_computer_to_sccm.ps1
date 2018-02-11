90c3b943112c9aae57fe563b915fa361fd42f8c5
zottelbeyer <zottelbeyer@users.noreply.github.com>
2018-02-11 15:50:31 +0100

    complete rework.

--- Add-Computer-to-SCCM/add_computer_to_sccm.ps1
+++ Add-Computer-to-SCCM/add_computer_to_sccm.ps1
@@ -1,388 +1,184 @@
-﻿# This Script imports a CSV and creates Datasets in SCCM
-# It then creates a file for the Collection to be updated. 
-# This script must be run on a SCCM Server with AD-Powershell Modules enabled.
-# This is What your CSV should look like:
-# Name, Mac, Collection, Archticture, OSVersion, Network,  TypID
-#
-# WARNING: Computername can not be longer than 15 Characters.
-# 
-# You can use this to automatically import with Varibales useable in an AIO Task Sequence.
+# Add Computer to SCCM
+# Imports Computer from CSV File into SCCM and adds predefined variables.
+# Creates Backup of CSV and writes logs.
+# Run this script as a scheduled task on your site-server
+# Version 1.1 - Uses CM PS-Cmdlet and allows for "Domain Join Only" Computers
+# Author: Klaus Steinhauer
+# Contact: contact@steinhkl.de
+# Date: 11.02.2018
+# USE AT YOUR OWN RISK!
 
-# Define Varibales
-[string]$SCCMComputer = "."
-[string]$smssite = "YOURSITECODE"
+Import-Module "E:\Program Files\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1" -Force -ErrorAction Stop
+
+# Base Vars
+$SiteCode = "CMG"
 $filepath="F:\SCCM_Import\CSVImport\"
 $backuppath="F:\SCCM_Import\CSVImportBackup\"
-$collectionfile="F:\SCCM_Import\RefreshCollection\"
-$targetOU="OU=Client_Install,OU=Client,DC=Install,DC=example,DC=com"
+$WMInamespace = "root\sms\site_CMG"
+$SiteServer = "s0001.yourdomain.com"
 
-# No editing beyond this point.
-[int]$varcount=0
-[int]$i=0
-[int]$clientscount=0
-$mac= $null
-$logentries=@()
-$clients=@()
-$groupmember=@()
-$runtime=Get-Date -format yyyyMMddHHmm
+$runtime=Get-Date -Format yyyyMMddHHmm
 $logfile=$backuppath+$runtime+".log"
-$errorlogfile=$backuppath+$runtime+"ERROR.log"
-$collectionfile=$collectionfile+$runtime+"Collection.txt"
-$Error.Clear()
+$resource = $false
+$DefaultCollection = "YOUR-DEFAULT-COLLECTION"
 
-#Active Directory Modul hinzufügen
-Import-Module -Name "ActiveDirectory" -Force -ErrorAction SilentlyContinue
-
-
-$refreshCol=@()
-$Class = "SMS_Site"
-$Method = "ImportMachineEntry"
-$MC = [WmiClass]"\\$SCCMComputer\ROOT\SMS\site_$($smssite):$Class"
-$error.clear()
-
-# Function for Creating a Logfile with timestamp
-Function writeLog ($logs)
-{
-    $time=(get-date).toLongTimestring()
-    add-content -path $logfile -Value "$time  $logs"
-}
-
-# Function for setting a varibale in SCCM
-function setvariable ($ResourceID,$variablename,$variablevalue)
-{
-    # If there is not yet a variable attached to the Computer Object
-	if ($varcount -lt 1) 
-	{
-		$objPCSet.psbase.properties["ResourceID"].value = $compResourceID
-		$objPCSet.psbase.properties["SourceSite"].value = $smssite
-		$objPCSet.MachineVariables = $objPCSet.MachineVariables + [WmiClass]"\\$SCCMComputer\ROOT\SMS\SITE_$($smssite):SMS_MachineVariable"
-		$machinevariables =  $objPCSet.MachineVariables
-		$machinevariables[($machinevariables.count)-1].Name = $variablename 
-		$machinevariables[($machinevariables.count)-1].value = $variablevalue
-		$objPCSet.MachineVariables = $machinevariables
-		$objPCSet=$objPCSet.put()
-		if ($?)
-		{
-            $varcount++
-			return $true
-		}
-		else
-		{
-			return $false
-		}
-		
-	}
-    # If there already are variables
-	else 
-	{
-		$objPCSet = [WmiClass]""
-		$objPCSet.psbase.Path ="\\$SCCMComputer\ROOT\SMS\SITE_$($smssite):SMS_MachineSettings"
-		$objPCSet = $objPCSet.createInstance()
-		$objPCSet.ResourceID = $compResourceID
-		$objPCSet.SourceSite = $smssite
-		$objPCSet.psbase.get() 
-		$objPCSet.MachineVariables = $objPCSet.MachineVariables + [WmiClass]"\\$SCCMComputer\ROOT\SMS\SITE_$($smssite):SMS_MachineVariable"
-		$machinevariables = $objPCSet.MachineVariables 
-		$machinevariables[($machinevariables.count)-1].Name = $variablename 
-		$machinevariables[($machinevariables.count)-1].value = $variablevalue 
-		$objPCSet.MachineVariables = $machinevariables 
-		$objPCSet=$objPCSet.put()
-		if ($?)
-		{
-            $varcount++
-			return $true
-		}
-		else
-		{
-			return $false
-		}
-	}
-}
-
-# Find computer in AD
-function findcomputer ($adclient)
-{
-	trap {$error.Clear();continue;return $false} 
-	if (($global:adclientpath=(get-adcomputer $adclient).DistinguishedName) -ne $null) {return $true}
-
-}
-
-# Check if target collection exists
-function findcollection ($strCollection)
-{
-    
-    if (!((Get-WmiObject -class "SMS_Collection" -namespace "root\SMS\Site_$smssite" -Filter "Name = '$strCollection'") -eq $null))
-    {
-        writelog ("Verbindung zur Sammlung $strCollection wurde aufgebaut.")
-        return $true
-    }
-    else 
-    {
-        writelog "ERROR: Sammlung $strCollection konnte nicht gefunden werden."
-        return $false
-    }
-}
-
-# Split the MAC
-function SplitMAC ($mac)
-{
-
-    $i=0
-    while ($i -lt 12)
-    {
-        if ($i -eq 0) {$mac=$mac.insert($i+2,":")}
-        else {$mac=$mac.insert(($i++)+3,":")}
-    $i=$i+2
-    }
-    $global:InParams.MACAddress = $mac
-    $global:delmac=$mac
- }
- 
-
-
-# Check Input Fileformat
-function checkFormat ($importfile)
-{
+# Check CSV file format.
+function checkFormat ($importfile){
     $checks=get-content $importfile.fullname
+
     foreach ($check in $checks)
     {
-        if (!$check.contains(",")) {return $false} 
-        else {$check=$check.split(",")}
-
-        #Anzahl der Parameter überprüfen
-        if ($check.count -ne 8 -or $check[1].length -ne 12) {return $false} 
-        else {continue}
+        # Is it a CSV?
+        if (!$check.split(",")) {
+            Write-Error "$($_.fullname) is not a CSV File"
+            return $false
+        }
+        else {
+            $check=$check.split(",")
+            # Do we have all parameters?
+            if ($check.count -ne 8 -or $check[1].length -ne 17) {
+                Write-Error "$($_.fullname) is not formated correctly."
+                return $false
+            }
+        }
+        Write-Information "$($_.fullname) was checked. Format Okay."
+        return $true
     }
-    return $true
-    
+
+}
+# find existing clients
+function searchClient($name,$mac){
+    $resource = $false
+    # search by name
+    $resource = Get-CMDevice -Name "$name"
+    if (!$resource){
+        Write-Information "Device Name $name not found."
+        Write-Information "Searching for MAC-Address."
+        # search by MAC. WMI Only.
+        $resourceID = (Get-WmiObject -Class SMS_R_SYSTEM -Namespace $WMInamespace -ComputerName $SiteServer | Where-Object {$_.MACAddresses -eq "$mac"}).ResourceID
+        # get resource by ID
+        if ($resourceID){
+            $resource = Get-CMDevice -ResourceId $resourceID
+        }else{
+            Write-Information "MAC-Address $mac not found."
+            Write-Information "Object doesn't seem to exist yet."
+        }
+    }
+    return $resource
 }
 
-# Get the files
+function addVarToClient($resource,$varname,$varvalue){
+    New-CMDeviceVariable -InputObject $resource -VariableName "$varname" -VariableValue "$varvalue" -IsMask 0 | Out-Null
+    Write-Information "The Variable $varname was set to $varvalue ."
+}
+
+# Get all CSV Files
 $importfiles= get-childitem -path $filepath -filter *.csv
-if ($importfiles -eq $null) 
-{
-    exit
-}
+# Exit if none are found
+if (!$importfiles) { exit }
+# Start logging
+Start-Transcript $logfile -IncludeInvocationHeader
 
+# Switch to SMS Drive
+If((Get-Location).Drive.Name -ne $SiteCode){
+        Try{Set-Location -path $SiteCode":" -ErrorAction Stop}
+        Catch{Throw "Unable to connect to Site $SiteCode. Ensure that the Site Code is correct and that you have access."}
+      }
 
-# Create Logfile
-New-Item -ItemType file -Path $logfile -force
-
-# Copy CSV File
-$importfiles| foreach {
+$importfiles| ForEach-Object {
+    Write-Information "Starting Processing of CSV File $_.name"
+    # Copy files to backuppath
     Copy-Item $_.fullname $backuppath -Force
-    writelog "$($_.fullname) was copied"
-   }
-writelog ("A copy of the CSV file(s) was created.")
+    Write-Information "$($_.fullname) was copied"
+
+    # Check format and begin processing
+    if (checkFormat $_){
+        # Header format of the CSV file.
+        $header = "Computername", "MACAddress", "Collection", "SMSTSOSArchitecture", "SMSTSOsVersion", "SMSTSNetwork", "SMSTSOSLanguage", "TypID"
+        # Write to Array
+        $array = Import-Csv $_.fullname -Header $header
+
+        # For each line in CSV
+        $array | ForEach-Object{
+            Write-Information "Searching for existing Device."
+            Write-Information "Computername: $($_.Computername) Mac-Address: $($_.MACAddress)"
+            # Search for existing client
+            $resource = searchClient -name "$($_.Computername)" -mac "$($_.MACAddress)"
+
+            #####################################################
+            # $Domain-Join #      $resource    #  !$resouce     #
+            #####################################################
+            #     YES      #       vars          # create, vars #
+            #     NO       #  del, create, vars  # create, vars #
+            #####################################################
+
+            # $Domain-Join -eq NO
+            if ($_.Collection -ne "AIO-PostDeployment"){
+                if($resource){
+                    #delete old client
+                    Remove-CMDevice -InputObject $resource -Force
+                    Write-Information "Client $($_.Computername) gelöscht"
+                    # Wait for Device to be removed to avoid race condition (which causes a lot of weird errors!)
+                    Start-Sleep -s 20
+                }
+
+                # add client
+                Write-Information "Importing Computer into SCCM"
+                Import-CMComputerInformation -ComputerName "$($_.Computername)" -MacAddress "$($_.MACAddress)" -CollectionName $DefaultCollection -ErrorAction Stop
+                Write-Information "Client $($_.Computername) with Mac-Address $($_.MACAddress) was created."
+                # give it some time.
+                while (!$ResourceID)
+                {
+	                $ResourceID = $(get-cmdevice -Name "$($_.Computername)").ResourceID
+	                Start-Sleep -Seconds 5
+                }
+            }
+            # $Domain-Join -eq YES
+            else {
+                # !$resource
+                if(!$resource){
+                    # add client
+                    Write-Information "Importing Computer into SCCM"
+                    Import-CMComputerInformation -ComputerName "$($_.Computername)" -MacAddress "$($_.MACAddress)" -CollectionName $DefaultCollection -ErrorAction Stop
+                    Write-Information "Client $($_.Computername) with Mac-Address $($_.MACAddress) was created."
+                    # give it some time.
+                    while (!$ResourceID)
+                    {
+	                    $ResourceID = $(get-cmdevice -Name "$($_.Computername)").ResourceID
+	                    Start-Sleep -Seconds 5
+                    }
+                }
+            }
+            # Find the newly created Client
+            $resource = searchClient -name "$($_.Computername)" -mac "$($_.MACAddress)"
+
+            # Add Vars
+            # This could be made prettier if you included the header in the CSV file and created a loop here
+            addVarToClient -resource $resource -varname "SMSTSOSArchitecture" -varvalue "$($_.SMSTSOSArchitecture)"
+            addVarToClient -resource $resource -varname "SMSTSOsVersion" -varvalue "$($_.SMSTSOsVersion)"
+            addVarToClient -resource $resource -varname "SMSTSNetwork" -varvalue "$($_.SMSTSNetwork)"
+            addVarToClient -resource $resource -varname "SMSTSOSLanguage" -varvalue "$($_.SMSTSOSLanguage)"
+            addVarToClient -resource $resource -varname "TypID" -varvalue "$($_.TypID)"
+
+            # Add Client to Collection
+            $collection = Get-CMDeviceCollection -Name "$($_.Collection)"
+
+            # check existing collection membership
+            $collectionmembership = Get-CMDeviceCollectionDirectMembershipRule -CollectionId $collection.CollectionID -Resource $resource
+            # if var is empty, computer is not in collection.
+            if (!$collectionmembership){
+                Add-CMDeviceCollectionDirectMembershipRule -CollectionId $collection.CollectionID -Resource $resource
+            }
 
 
-#Format der ImportDatei überprüfen
-
-#Computerdateien importieren
-foreach ($importfile in $importfiles)
-{
-    
-    if (checkformat $importfile) 
-    {
-        $clients+= Get-Content -Path $importfile.fullname
+        }
+        Write-Information "$($_.fullname) was processed and is being deleted."
+        Remove-Item $_.fullname
     }
-    else
-    {
-        writelog "ERROR: The File $importfile is invalid. Check Formatting."
-        writelog "This Script will stop now."
+    else{
+        Write-Error "Format Error in $_.fullname"
         exit
     }
 }
 
-
-$directruleClass = [WMIClass] "root\SMS\Site_$($smssite):SMS_CollectionRuleDirect"
-$directruleInstance = $directruleClass.CreateInstance() 
-$directruleInstance.ResourceClassName = "SMS_R_System"
-
-# Creating the Computer Object in SCCM
-Foreach ($client in $clients)
-{
-    $i=0
-    # get the MAC
-    $client=$client.Split(",")
-    $InParams = $mc.psbase.GetMethodParameters($Method)
-    # split it
-    splitmac ($Client[1])
-    # use it as NetbiosName
-	$InParams.NetbiosName = $Client[0]
-	$objcomputer=$client[0]
-    $runclient=$client[0]
-    writelog ("Now working on '$runclient'")
-	# Check if there is a USMT entry
-	$objMig = Get-WmiObject -namespace "root\SMS\Site_$($smssite)" -Query "select * from SMS_StateMigration where Restorename = '$($client[0])'"
-´	# 
-	if ($objmig -ne $null -and $obj.MigrationType -eq 1)
-	{
-		writelog ("There is a USMT record for $($objmig.sourcename) .")
-		writelog ("I will not delete this Object for you!")
-        continue
-	}
-
-	# Delete existing Computer with the same name
-    $objcomputer = Get-WmiObject -class "SMS_R_System" -namespace "root\SMS\Site_$($smssite)" -Filter "Name = '$($client[0])'"
-	if ($objComputer -ne $null) 
-     {
-       foreach ($obj in $objcomputer)
-        {
-            $obj.delete()
-        }
-        if ($?)
-		{
-			writelog ("WARNING: Object $runclient was deleted based on it's name in SCCM.")
-		}
-		else
-		{
-			writelog ("ERROR: Object $runclient could not be deleted (based on it's name) in SCCM.")
-		}
-    }
-    # Delete existing Computer with the same MAC
-    $objcomputermac = Get-WmiObject -class "SMS_R_System" -namespace "root\SMS\Site_$($smssite)" -Filter "MACAddresses = '$delmac'"
-    if ($objComputermac -ne $null) 
-    {
-        foreach ($objmac in $objcomputermac)
-        {
-            $objmac.delete()
-        }
-		if ($?)
-		{
-        	writelog ("WARNING: Object $runclient was deleted based on it's MAC in SCCM.")
-		}
-		else
-		{
-			writelog ("ERROR: Object $runclient could not be deleted (based on it's MAC) in SCCM.")
-		}
-   }
-    
-	# Create Computer in SCCM
-	$objComputer = $mc.PSBase.InvokeMethod($Method, $inParams, $Null)
-	$directruleInstance.ResourceID = $objcomputer.ResourceId
-	if ($?)
-	{
-    	writelog ("Object $runclient was created or updated with ResourceId " + $objcomputer.ResourceId)
-    }
-	else
-	{
-		writelog ("ERROR: Object $runclient could not be created or updated.")
-		writelog ("ERROR: Aborting work on $runclient .")
-		continue		
-	}
-	
-	$compResourceID=$objcomputer.ResourceId
-   
-	# Search for Computer Object in AD and extract Groups
-	$adclient=$client[0]
-	if ((findcomputer $adclient)) 
-		{
-			$comp= (get-adcomputer $adclient -properties memberof)
-
-	           foreach ($group in $comp.memberof)
-	               {
-		              if ((get-adgroup $group).samaccountname -like "sccm*")
-                      {
-                      $groupmember+=(get-adgroup $group).samaccountname
-                      }
-	               }
-		} 
-	else # Create new Computer Object
-	{
-		New-ADComputer -Name $adclient -Path $targetOU
-		if ($?)
-		{
-			writelog ("Computer Object $adclient was created in AD.")
-		}
-		else
-		{
-			writelog ("ERROR: Computer Object $adclient could not be created in AD.")
-		}
-	}
-
-	# Define SCCM Computer Object
-	$objPCSet = [WmiClass]"" 
-	$objPCSet.psbase.Path ="\\$SCCMComputer\ROOT\SMS\SITE_$($smssite):SMS_MachineSettings" 
-	$objPCSet =  $objPCSet.CreateInstance()
-	$varcount=0
-	foreach ($ClientProperty in $Client)        
-	{           
-        # Set the Collection
-        If ($i -eq 2){
-            if (findcollection ($ClientProperty))
-                {
-	            (Get-WmiObject -class "SMS_Collection" -namespace "root\SMS\Site_$smssite" -Filter "Name = '$ClientProperty'").AddMembershipRule($directruleInstance)
-                if ($?)
-                {
-                        writelog ("Object $runclient was added to Collection $ClientProperty.")
-                }
-	            else
-	            {
-			            writelog ("ERROR: Object $runclient could not be added to Collection $ClientProperty.")
-	            }
-            }
-        }
-        # Set all other Variables
-		elseIf ($i -gt 2 -and $i -lt 7) 
-	    {
-            switch($i)
-            {
-                3 {$variableName="SMSTSOsArchitecture"}
-                4 {$variableName="SMSTSOsVersion"}
-                5 {$variableName="SMSTSNetwork"}
-                6 {$variableName="TYPID"}
-            }
-
-            if (setvariable $compResourceID $variablename $ClientProperty)
-			{
-                writelog ("Variable $variablename was set to $ClientProperty for Object $runclient.")
-			}
-			else
-			{
-				writelog ("ERROR: Variable $variablename could not be set to $ClientProperty for Object $runclient.")
-			}
-
-		}
-		$i++
-        Continue
-	}
-	writelog ("Client $runclient was successfully processed.")
-    $clientscount++
-}
-
-# Create Collection File
-IF ($groupmember -ne $null)
-{
-    $refreshCol= ($groupmember| select -Unique)
-    add-content -path (New-Item -ItemType file -Path $collectionfile -force) -value $refreshCol
-    if ($?)
-	{
-		writelog ("Collectionfile $collectionfile was created.")
-	}
-	else
-	{
-		writelog ("ERROR: Collectionfile $collectionfile could not be created.")
-	}
-}
-
-# Delete Imported File
-get-childitem -path $filepath -filter *.csv| Remove-Item -Force
-writelog "Importfiles were deleted"
-
-if (!$error)
-{
-    writelog ("Script was successfully completed: $clientscount Computers were imported.")
-}
-else
-{
-    writelog ("There where errors during processing. Please check Errorlogfile: $errorlogfile.")
-}
-
-
-# Write errors to file
-if ($error -ne $null)
-{
-    $Error| Out-File -filepath $errorlogfile -force
-	add-content -path $errorlogfile -Value "There where errors during processing. Please check Errorlogfile: $logfile."
-}
+Stop-Transcript
